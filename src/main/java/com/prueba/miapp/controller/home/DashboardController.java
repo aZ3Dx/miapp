29ca6bc84a703128616a3d455cca6a8b3d21e02e
Jean Herrera <jeankevinherrera@gmail.com>
2023-02-18 00:18:54 -0500

    Otro

--- src/main/java/com/prueba/miapp/controller/home/DashboardController.java
+++ src/main/java/com/prueba/miapp/controller/home/DashboardController.java
@@ -1,24 +1,28 @@
 package com.prueba.miapp.controller.home;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@PreAuthorize("hasRole('Administrador') or hasRole('Cliente')")
 @Controller
 public class DashboardController {
     
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model) {
+        System.out.println("Mostrando dashboard");
         return "dashboard";
     }
     
-    @GetMapping("/dashboard/usuarios")
-    public String usuarios() {
-        return "dashboard/usuarios";
-    }
+    // @GetMapping("/dashboard/usuarios")
+    // public String usuarios() {
+    //     return "dashboard/usuarios";
+    // }
     
-    @GetMapping("/dashboard/info")
-    public String info() {
-        return "dashboard/info";
-    }
+    // @GetMapping("/dashboard/info")
+    // public String info() {
+    //     return "dashboard/info";
+    // }
 
 }
